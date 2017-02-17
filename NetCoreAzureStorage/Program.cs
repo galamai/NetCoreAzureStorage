aa0b80d236f7780289be8edfa6ec5d6aced8d99d
Евгений Галамай < >
2017-02-17 22:16:24 +0200

    Добавьте файлы проекта.

---
+++ NetCoreAzureStorage/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NetCoreAzureStorage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
